52d5bc1fbc9d29a83617197c1d684f3a831a0817
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-04-10 19:04:04 +0530

    script.js updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () =>
-    navigator.serviceWorker.register('sw.js',{scope:"/"})
+    navigator.serviceWorker.register('sw.js',{scope:"/Ariyippukal-Online-Front-End/"})
       .then(registration => console.log('Service Worker registered'))
       .catch(err => 'SW registration failed'));
 }
